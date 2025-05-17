e34f9e111c62a7656fe0bdaa4156a56f64558606
aaronoates <aaronoates2003@gmail.com>
2025-05-17 16:47:15 -0230

    Questions 1 and 2 complete

--- a1_files/parallel.cpp
+++ a1_files/parallel.cpp
@@ -7,18 +7,48 @@
 // std:: as a prefix.  Alternatively, you could use: using namespace std;
 using std::thread, std::unique_ptr, std::make_unique, std::ref;
 
+
+
+
 void sortBucketsParallel1(int **buckets, int numBuckets, int *bucketSizes)
 {
     // STUDENTS: COMPLETE THIS FUNCTION AS PER THE COMMENT IN parallel.h.
+    thread *thr[numBuckets];
 
+    for (int i = 0; i < numBuckets; i++) {
+        thr[i] = new thread(qsort, buckets[i], bucketSizes[i], sizeof(int), compare);
+        thr[i]->join();
+        delete thr[i];
+    }
+    
 }
 
 // STUDENTS: YOU CAN DEFINE A FUNCTOR RIGHT HERE FOR USE IN sortBucketsParallel2.
 
+struct sortoneBucket {
+    int *bucket = nullptr;
+    int bucketSize;
+
+    void operator()() {
+        qsort(bucket, bucketSize, sizeof(int), compare);
+    }
+        
+};
 
 void sortBucketsParallel2(int **buckets, int numBuckets, int *bucketSizes)
 {
     // STUDENTS: COMPLETE THIS FUNCTION AS PER THE COMMENT IN parallel.h.
     // IF YOU USE A FUNCTOR, YOU CAN DEFINE IT ABOVE.
+    unique_ptr<thread> threads[numBuckets];
+    sortoneBucket functors[numBuckets];
 
+    for (int i = 0; i < numBuckets; ++i) {
+        functors[i].bucket = buckets[i];
+        functors[i].bucketSize = bucketSizes[i];
+        threads[i] = make_unique<thread>(ref(functors[i]));
+    }
+
+    for (int i = 0; i < numBuckets; ++i) {
+        threads[i]->join();
+    }
 }
